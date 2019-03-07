22f629cd1fe129f2284483a390198733ee35e0b0
nipunsingal <500069052@stu.upes.ac.in>
2019-03-07 10:13:29 +0530

    file added

---
+++ file1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    int sum, sub, mult, mod;
+    float div;
+
+
+    printf("Enter any two numbers: ");
+    scanf("%d%d", &num1, &num2);
+
+
+    sum = num1 + num2;
+    
+   
+
+
+    printf("SUM = %d\n", sum);
+   
+
+    return 0;
+}
+
+
---
+++ file2.c
@@ -0,0 +1,36 @@
+i#include<stdio.h>                              /*made by nipun singal himself*/
+
+int main ()
+{                                             /*declaring the variables*/
+    int [20];                                  /*array is declared*/
+    char ans,ans2;
+    char name[20];
+    int i=0,b;
+printf("                  ***********Welcome to the C language Quiz****************\n");            /*program begins*/
+printf("\n        Round1                     Round2                         Round3\n");
+printf("       True/False (Q1-Q10)    Multiple Choice(Q11-Q15)       Multiple answers(Q16-Q20)\n");
+printf("\n  ****Total marks=20****\n   Their is negative marking\n\n\n\n");
+printf("Candidate name-");
+scanf("%s",name);
+printf("\n\n");
+printf("Lets start the quiz\n\n");
+
+for(a=0;a<=100;a++)                                /*for loop is used so that we can go to any question anytime*/
+{         if(c==0)                                     /*if user enters 0 program comes out of loop and shows score*/
+           {break;}
+             printf("enter the question number you want to go or enter 0 to end the quiz:");
+             scanf("%d",&i);
+
+                  switch(i)                           /*switch case starts,it contains the quiz questions*/
+
+                  {
+
+                    case 1:printf("\nRound1 Begins.......Write T/F\n\n");
+                     printf("Q1. C language is developed by Dennis M. Ritchie?\n answer--");
+                     scanf("%s",&ans);                             /*this will store the users answer*/
+                     if(ans=='T'||ans=='t')
+                     {scr[0]=1;}                       /*if users answer is correct it will store 1 in the scr array*/
+                     else                               /*otherwise zero in scr array*/
+                     {scr[0]=-1;}
+                     printf("\n\n");
+                     break;
